4fcacf9ef9ae4b45cfdbeb9f8da4923fa390206d
hamet 🦖 <hamet.gh@gmail.com>
2021-10-31 19:28:42 +0330

    comment added to controller methods

--- src/lib/controllers/transactionController.ts
+++ src/lib/controllers/transactionController.ts
@@ -1,7 +1,36 @@
 import { Request, Response } from "express";
 
-export const add = (req: Request, res: Response) => {};
+/**
+ * find transaction
+ * @param req Request
+ * @param res Response
+ */
 export const find = (req: Request, res: Response) => {};
+
+/**
+ * find transaction by id
+ * @param req Request
+ * @param res Response
+ */
 export const findOne = (req: Request, res: Response) => {};
+
+/**
+ * insert new transaction
+ * @param req Request
+ * @param res Response
+ */
+export const add = (req: Request, res: Response) => {};
+
+/**
+ * update transaction by id
+ * @param req Request
+ * @param res Response
+ */
 export const update = (req: Request, res: Response) => {};
+
+/**
+ * remove transaction by id
+ * @param req Request
+ * @param res Response
+ */
 export const remove = (req: Request, res: Response) => {};
